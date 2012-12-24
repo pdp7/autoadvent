5ab3eee6c012c54b1507dbf6705934c54f7fc477
Drew Fustini <github@pdp7.com>
2012-12-24 00:35:43 -0600

    Update autoadvent.sh
    add note about Rev 2.0 I2C bus

--- autoadvent.sh
+++ autoadvent.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 LOG=/var/log/advent.log
+#if you have a Rev 2.0 Pi, then you'll need to change "i2c-0" below to "i2c-1" as the I2C bus is on the GPIO header has changed between Rev 1 and 2
 echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-0/new_device
 hwclock &> $LOG
 date &>> $LOG
